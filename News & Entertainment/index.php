66a979a35754d0da47d12bfff704229471af8bfa
Amar Sharma <amarraj364@gmail.com>
2024-01-08 23:05:31 +0530

    file structure update

---
+++ News & Entertainment/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset='utf-8'>
+    <meta http-equiv='X-UA-Compatible' content='IE=edge'>
+    <title>Info Jharkhand</title>
+    <meta name='viewport' content='width=device-width, initial-scale=1'>
+    <link rel='stylesheet' type='text/css' media='screen' href='main.css'>
+    <script src='main.js'></script>
+</head>
+<body>
+    <h1 >wellcom in this site Info Jharkhand</h1>
+    
+</body>
+</html>
